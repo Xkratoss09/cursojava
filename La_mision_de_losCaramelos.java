864a35e91791bfc2308f48db8e891d777cfe347a
Xkratoss09 <jeanescobarpinzon.est@comfandi.edu.co>
2025-10-14 18:11:30 -0500

    Add files via upload

---
+++ La_mision_de_losCaramelos.java
@@ -0,0 +1,29 @@
+
+package com.mycompany.la_mision_de_loscaramelos;
+
+/**
+ *
+ * @author Jean pierre escobar pinzon
+ */
+public class La_mision_de_losCaramelos {
+
+    public static void main(String[] args) {
+        
+        //crear variable 
+        String username="camila";
+        int cofreUno=8;
+        int cofreDos=12;
+        int sumaDulces;
+        
+        //proceso
+        sumaDulces=cofreUno + cofreDos;
+        
+        
+        
+        
+        
+        
+        //salida
+        System.out.print(username + "ahora tiene en total de" + sumaDulces + "caramelos magicos");
+    }
+}
---
+++ ejercicio2.java
@@ -0,0 +1,28 @@
+
+package com.mycompany.la_mision_de_loscaramelos;
+
+/**
+ *
+ * @author Jean pierre escobar pinzon
+ * 
+ */
+public class ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double retoUno=3.5;
+        double retoDos=4.2;
+        double retoTres=2.8;
+        
+        //proceso
+        double promedioPuntos;
+        promedioPuntos=(retoUno + retoDos + retoTres)/3;
+        
+        
+        //salida
+        System.out.print( "tu promedio final es" + promedioPuntos + "!Niuvel Superado¡");
+    }
+    
+}
